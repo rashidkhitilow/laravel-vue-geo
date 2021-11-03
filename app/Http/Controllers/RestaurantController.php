cf1504c23a71aa2066a0d36ee47d29d88ed9b1ee
Rashid Khitilov <rashid.khitilov@isbis.local>
2021-11-03 09:14:02 +0400

    First commit

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    public function index(){
+        return Restaurant::all();
+    }
+}
---
+++ database/migrations/2021_09_23_114110_create_restaurants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('hours');
+            $table->decimal('latitude', 8,6);
+            $table->decimal('longitude', 9,6);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
